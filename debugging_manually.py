53e70afe4a056ad7e859beba3ffb1821c03628a6
Minjule <minjinsor.minjee0909@gmail.com>
2023-11-21 18:30:13 +0800

    update

--- debugging_manually.py
+++ debugging_manually.py
@@ -1,3 +1,4 @@
 from import_data import create_spectrogram
+from slice_spectogram import slice_spect
 
 create_spectrogram(verbose=1, mode="Train")
--- import_data.py
+++ import_data.py
@@ -18,8 +18,8 @@ These successive frames are then concatenated into a matrix to form the spectrog
 """
 def create_spectrogram(verbose=0, mode=None):
     if mode == "Train":
-        if os.path.exists('Train_Spectogram_Images'):
-            return
+        #if os.path.exists('Train_Spectogram_Images'):
+         #   return
         # Get Genres and Track IDs from the tracks.csv file
         filename_metadata = "D:/projects/Music-Reccomendation-Dataset/fma_metadata/tracks.csv"
         tracks = pd.read_csv(filename_metadata, header=2, low_memory=False)
@@ -32,11 +32,11 @@ def create_spectrogram(verbose=0, mode=None):
 
         folder_sample = "D:/projects/Music-Reccomendation-Dataset/fma_small"
         directories = [d for d in os.listdir(folder_sample) if os.path.isdir(os.path.join(folder_sample, d))]
-        counter = 0
+        counter = 4470
         if(verbose > 0):
             print("Converting mp3 audio files into mel Spectograms ...")
-        if not os.path.exists('Train_Spectogram_Images'):
-            os.makedirs('Train_Spectogram_Images')
+        #if not os.path.exists('Train_Spectogram_Images'):
+        #    os.makedirs('Train_Spectogram_Images')
         for d in directories:
             label_directory = os.path.join(folder_sample, d)
             file_names = [os.path.join(label_directory, f) for f in os.listdir(label_directory) if f.endswith(".mp3")]
@@ -51,24 +51,24 @@ def create_spectrogram(verbose=0, mode=None):
                 print(track_id)
                 track_id = re.sub(r'^0+', '', track_id)
                 print(track_id)
-
-                track_index = list(tracks_id_array).index(int(track_id))
-                if(str(tracks_genre_array[track_index, 0]) != '0'):
-                    print(f)
-                    y, sr = librosa.load(f)
-                    melspectrogram_array = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=8000)
-                    mel = librosa.power_to_db(melspectrogram_array)
-                    # Length and Width of Spectogram
-                    fig_size = plt.rcParams["figure.figsize"]
-                    fig_size[0] = float(mel.shape[1]) / float(100)
-                    fig_size[1] = float(mel.shape[0]) / float(100)
-                    plt.rcParams["figure.figsize"] = fig_size
-                    plt.axis('off')
-                    plt.axes([0., 0., 1., 1.0], frameon=False, xticks=[], yticks=[])
-                    librosa.display.specshow(mel, cmap='gray_r')
-                    plt.savefig("Train_Spectogram_Images/"+str(counter)+"_"+str(tracks_genre_array[track_index,0])+".jpg", bbox_inches=None, pad_inches=0)
-                    plt.close()
-                    counter = counter + 1
+                if(int(track_id)>99134):
+                    track_index = list(tracks_id_array).index(int(track_id))
+                    if(str(tracks_genre_array[track_index, 0]) != '0'):
+                        print(f)
+                        y, sr = librosa.load(f)
+                        melspectrogram_array = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=8000)
+                        mel = librosa.power_to_db(melspectrogram_array)
+                        # Length and Width of Spectogram
+                        fig_size = plt.rcParams["figure.figsize"]
+                        fig_size[0] = float(mel.shape[1]) / float(100)
+                        fig_size[1] = float(mel.shape[0]) / float(100)
+                        plt.rcParams["figure.figsize"] = fig_size
+                        plt.axis('off')
+                        plt.axes([0., 0., 1., 1.0], frameon=False, xticks=[], yticks=[])
+                        librosa.display.specshow(mel, cmap='gray_r')
+                        plt.savefig("Train_Spectogram_Images/"+str(counter)+"_"+str(tracks_genre_array[track_index,0])+".jpg", bbox_inches=None, pad_inches=0)
+                        plt.close()
+                        counter = counter + 1
         return
 
     elif mode == "Test":
